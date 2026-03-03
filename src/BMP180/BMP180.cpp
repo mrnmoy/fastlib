9b8ea70a7049754adf12f2bc8b599a25b7a58251
MRINMOY MONDAL <mm2922894@gmail.com>
2026-03-03 19:46:15 +0530

    Update BMP180
    replaced exponents to constants

--- src/BMP180/BMP180.cpp
+++ src/BMP180/BMP180.cpp
@@ -12,12 +12,6 @@ void BMP180::update() {
   temp = getTemp(b5);
   press = getPress(b5, cal);
   alt = getAlt(press);
-  Serial.print(" temp: ");
-  Serial.print(temp);
-  Serial.print(" press: ");
-  Serial.print(press);
-  Serial.print(" alt: ");
-  Serial.println(alt);
 }
 
 CalParams BMP180::getCP() {
@@ -35,29 +29,6 @@ CalParams BMP180::getCP() {
   cal.mb = (calData[16] << 8) | calData[17];
   cal.mc = (calData[18] << 8) | calData[19];
   cal.md = (calData[20] << 8) | calData[21];
-
-  Serial.print("ac1: ");
-  Serial.print(cal.ac1);
-  Serial.print(" ac2: ");
-  Serial.print(cal.ac2);
-  Serial.print(" ac3: ");
-  Serial.print(cal.ac3);
-  Serial.print(" ac4: ");
-  Serial.print(cal.ac4);
-  Serial.print(" ac5: ");
-  Serial.print(cal.ac5);
-  Serial.print(" ac6: ");
-  Serial.print(cal.ac6);
-  Serial.print(" b1: ");
-  Serial.print(cal.b1);
-  Serial.print(" b2: ");
-  Serial.print(cal.b2);
-  Serial.print(" mb: ");
-  Serial.print(cal.mb);
-  Serial.print(" mc: ");
-  Serial.print(cal.mc);
-  Serial.print(" md: ");
-  Serial.println(cal.md);
   return cal;
 } 
 int32_t BMP180::getUT() {
@@ -88,13 +59,13 @@ int32_t BMP180::getB5(const CalParams cal) {
   // int32_t x2 = (cal.mc << 11) / (x1 + cal.md);
   // return x1 + x2;
 
-  int32_t x1 = ((getUT() - cal.ac6) * cal.ac5) / pow(2, 15);
-  int32_t x2 = (cal.mc * pow(2, 11)) / (x1 + cal.md);
+  int32_t x1 = ((getUT() - cal.ac6) * cal.ac5) / 32768;
+  int32_t x2 = (cal.mc * 2048) / (x1 + cal.md);
   return x1 + x2;
 }
 float BMP180::getTemp(int32_t b5) {
   // return (float)((b5 + 8) >> 4) / 10.0;
-  return ((b5 + 8) / pow(2, 4)) / 10.0;
+  return ((b5 + 8) / 16) / 10.0;
 }
 float BMP180::getPress(int32_t b5, const CalParams cal) {
   int32_t x1, x2, x3, b3, b6, p;
@@ -118,21 +89,21 @@ float BMP180::getPress(int32_t b5, const CalParams cal) {
   // return (p + ((x1 + x2) + 3791)) >> 4;
 
   b6 = b5 - 4000;
-  x1 = (cal.b2 * (b6 * b6 / pow(2, 12))) / pow(2, 11);
-  x2 = cal.ac2 * b6 / pow(2, 11);
+  x1 = (cal.b2 * (b6 * b6 / 4096)) / 2048;
+  x2 = cal.ac2 * b6 / 2048;
   x3 = x1 + x2;
   b3 = (((cal.ac1 * 4 + x3) << oss) + 2) / 4;
-  x1 = cal.ac3 * b6 / pow(2, 13);
-  x2 = (cal.b1 * (b6 * b6 / pow(2, 12))) / pow(2, 10);
-  x3 = ((x1 + x2) + 2) / pow(2, 2);
-  b4 = cal.ac4 * (uint32_t)(x3 + 32768) / pow(2, 15); 
+  x1 = cal.ac3 * b6 / 8192;
+  x2 = (cal.b1 * (b6 * b6 / 4096)) / 1024;
+  x3 = ((x1 + x2) + 2) / 4;
+  b4 = cal.ac4 * (uint32_t)(x3 + 32768) / 32768; 
   b7 = ((uint32_t)getUP() - b3) * (50000 >> oss);
   if (b7 < 0x80000000) p = (b7 * 2) / b4;
   else p = (b7 / b4) * 2;
-  x1 = (p / pow(2, 8)) * (p / pow(2, 8));
-  x1 = (x1 * 3038) / pow(2, 16);
-  x2 = (-7357 * p) / pow(2, 16);
-  return p + (x1 + x2 + 3791) / pow(2, 4);
+  x1 = (p / 256) * (p / 256);
+  x1 = (x1 * 3038) / 65536;
+  x2 = (-7357 * p) / 65536;
+  return p + (x1 + x2 + 3791) / 16;
 }
 float BMP180::getAlt(int32_t p) {
   // return 44330.0 * (1.0 - pow(p / 1013.25, 1 / 5.255));
